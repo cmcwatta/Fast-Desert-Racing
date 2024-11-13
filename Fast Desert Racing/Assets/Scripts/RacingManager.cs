e018832663995a0ba37ae758f45611853a225232
Dylan <changliangdylan@gmail.com>
2024-11-13 13:46:19 -0500

    Test

--- Fast Desert Racing/Assets/Scripts/RacingManager.cs
+++ Fast Desert Racing/Assets/Scripts/RacingManager.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RacingManager : MonoBehaviour
@@ -15,16 +16,31 @@ public class RacingManager : MonoBehaviour
 
     void Start()
     {
-        if (GameData.CarPrefab)
-        {
-            _car = Instantiate(GameData.CarPrefab, originalCar.transform.position, originalCar.transform.rotation);
-            Destroy(originalCar);
-            Destroy(GameData.CarPrefab);
-        }
-        else _car = originalCar;
+        //if (GameData.CarPrefab)
+        //{
+        //    _car = Instantiate(GameData.CarPrefab, originalCar.transform.position, originalCar.transform.rotation);
+        //    Destroy(originalCar);
+        //    Destroy(GameData.CarPrefab);
+        //}
+        //else _car = originalCar;
 
-        vCam.LookAt = _car.transform;
-        vCam.Follow = _car.transform;
-        _car.GetComponent<Car>().allowUse = true;
+        //vCam.LookAt = _car.transform;
+        //vCam.Follow = _car.transform;
+        //_car.GetComponent<Car>().allowUse = true;
+    }
+
+    private void Update()
+    {
+        Alteruna.Avatar[] avatars = FindObjectsOfType<Alteruna.Avatar>();
+
+        foreach (Alteruna.Avatar avatar in avatars)
+        {
+            if (avatar.IsMe)
+            {
+                vCam.LookAt = avatar.transform;
+                vCam.Follow = avatar.transform;
+                avatar.GetComponent<Car>().allowUse = true;
+            }
+        }
     }
 }
