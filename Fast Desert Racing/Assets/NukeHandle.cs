24fed8438b56207969b9a105d0da8413707345cf
Dylan <changliangdylan@gmail.com>
2024-12-09 07:12:30 -0500

    Finished Nuke uwu

--- Fast Desert Racing/Assets/NukeHandle.cs
+++ Fast Desert Racing/Assets/NukeHandle.cs
@@ -15,6 +15,16 @@ public class NukeHandle : MonoBehaviour
     [SerializeField]
     private GameObject nukeObject;
 
+    [SerializeField]
+    private GameObject nukeExplosion;
+
+    [SerializeField]
+    private Transform nukeExplosionTransform;
+
+    [SerializeField]
+    private Light globalLight;
+
+    private bool exploded;
     public void StartNuke()
     {
         nukeScene.SetActive(true);
@@ -24,17 +34,33 @@ public class NukeHandle : MonoBehaviour
 
         Nuke.OnExplode += () =>
         {
-            ShowError("Boom! System destroyed. Goodbye, cruel world!", "Critical Error: Nuke!");
+            if (exploded) return;
+            Instantiate(nukeExplosion, nukeExplosionTransform.position, Quaternion.identity);
+
+            StartCoroutine(IncreaseBright());
+            exploded = true;
         };
     }
 
-    // Import the MessageBox function from user32.dll
+    private IEnumerator IncreaseBright()
+    {
+        while(true)
+        {
+            if (globalLight.intensity > 100) break;
+
+            yield return new WaitForSeconds(0.1f);
+            globalLight.intensity += 0.5f;
+        }
+        if (!Application.isEditor) ShowError("Boom! System destroyed. Goodbye, cruel world!", "Critical Error: Nuke!");
+    }
+
     [DllImport("user32.dll", CharSet = CharSet.Unicode)]
     private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
 
     private void ShowError(string message, string title)
     {
         MessageBox(IntPtr.Zero, message, title, 0x10);
+        MessageBox(IntPtr.Zero, "THANKS FOR PLAYING!", "THANKS", 0x10);
         Application.Quit();
     }
 }
